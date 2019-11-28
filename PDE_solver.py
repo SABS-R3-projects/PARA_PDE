f429629bf2c744c3ee9f684220956b7b1081a407
Pavan Chaggar <pavanchaggar@Pavans-MacBook-Pro.local>
2019-11-28 18:20:26 +0000

    adding PDE_solver.py

---
+++ PDE_solver.py
@@ -0,0 +1,42 @@
+'''
+Class to create and solve Fitzhugh-Nagumo equation
+
+'''
+
+import numpy as np
+import scipy
+import scipy.sparse
+import scipy.sparse.linalg
+import matplotlib.pyplot as plt
+import time
+from matplotlib.animation import FuncAnimation
+
+class FitzHugh_Nagumo_solver(object):
+    def __init__ (self):
+        pass 
+    
+    def fitzhugh_nagumo(self, x_0, a_0: float):
+        self.x_0 = x_0
+        self.a_0 = a_0
+        alpha = self.a_0
+        size = len(self.x_0)
+        u = np.zeros(size)
+
+        for i in range(size):
+            u[i] = 0.5*(1 + alpha) + 0.5*(1- alpha)*(np.tanh((np.sqrt(2)*(1-alpha)*x[i])/4))
+            return u
+
+
+    def __laplace_matrix(self, N):
+        e = np.ones(N)
+        diagonals = [e, -2*e, e]
+        offsets = [-1, 0, 1]
+        L = scipy.sparse.spdiags(diagonals, offsets, N, N) / h**2
+        return L
+
+    def solver(self, parameter_list):
+        pass
+
+    def plot(self, parameter_list):
+        pass
+
