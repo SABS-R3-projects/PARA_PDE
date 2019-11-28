4ce1aac5a66014bdc1b175e60dc073dd37f5232b
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2019-11-28 16:09:48 +0000

    :panda_face: documentation

--- fitzhuge_nagumo_solver.py
+++ fitzhuge_nagumo_solver.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import time
 from matplotlib.animation import FuncAnimation
 
-def construct_laplace_matrix_1d(N, h):
+def construct_laplace_matrix_1d(N: int, h: float):
     '''Method to construct a sparse laplace matrix using scipy
 
     param N: dimensions of the laplace matirx
@@ -35,20 +35,23 @@ def solve_fitzhuge_nagumo():
     L = construct_laplace_matrix_1d(N, h)
     bc = np.concatenate(([0], np.zeros(N-2), [1]))/h**2 # boundary conditions
 
-    def initial_conditions(x,alpha = 0.2):
+    def initial_conditions(x: list, alpha: float = 0.2):
         '''Method to set the initial conditions of the Nagumo equation for iterative solving
 
-        param x: position coordinate
+        param x: list of position coordinate
         param alpha: alpha is a constant of the equation which should obey 0 < alpha < 0.5 
                      the default value 0.2 is used if not specified
         return: initial condition with given parameters
         '''
-        return 0.5*(1 + alpha) + 0.5*(1- alpha)*(np.tanh((np.sqrt(2)*(1-alpha)*x)/4))
 
-    size = len(x) 
-    u = np.zeros(size)
-    for i in range(size):
-        u[i] = (initial_conditions(x[i],0.2))
+        size = len(x) 
+        u = np.zeros(size)
+        for i in range(size):
+            u[i] = 0.5*(1 + alpha) + 0.5*(1- alpha)*(np.tanh((np.sqrt(2)*(1-alpha)*x[i])/4))
+
+        return u
+
+    u = initial_conditions(x,0.2)
 
     plt.plot(x,u)
     plt.show()
