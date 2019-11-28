7ac00374aa8c7d4fb8dd64fb754b7d7e672b4681
Pavan Chaggar <pavanchaggar@Pavans-MacBook-Pro.local>
2019-11-28 20:03:52 +0000

    added solver function

--- PDE_solver.py
+++ PDE_solver.py
@@ -17,7 +17,7 @@ class FitzHugh_Nagumo_solver(object):
     
     def fitzhugh_nagumo(self, x_0, a_0: float):
 
-        self.x_0 = x_0
+        self.x_range = x_0
         self.a_0 = a_0
         alpha = self.a_0
         size = len(self.x_0)
@@ -25,7 +25,9 @@ class FitzHugh_Nagumo_solver(object):
 
         for i in range(size):
             u[i] = 0.5*(1 + alpha) + 0.5*(1- alpha)*(np.tanh((np.sqrt(2)*(1-alpha)*x[i])/4))
-            return u
+
+        self.x_0 = u
+        return u
 
 
     def __laplace_matrix(self):
@@ -38,11 +40,22 @@ class FitzHugh_Nagumo_solver(object):
 
         return self.L
 
-    def FN_solver(self, boundary_conditions, step_size):
+    def FN_solver(self, initial_conds, boundary_conditions, step_size, time_steps):
         self.h = step_size
+        self.h_N = time_steps
         self.bc = boundary_conditions
 
-        
+        u = np.empty(len(self.x_range,self.h_N))
+        u[:,0] = initial_conds
+        k = 0.2 * h**2
+        time_steps = 8000
+        #int(np.ceil(42/k)/10) DO WE NEED THIS?!
+
+        for i in range(1:time_steps-1):
+            u[:,i] = u[:,i-1] + k*( eps*(L@u[:,i-1] + bc) + (u[:,i-1]**2 - u[:,i-1]**3 - a*u[:,i-1] + a*u[:,i-1]**2) )
+            u[:] = u_new
+
+        return u
 
     def plot(self, parameter_list):
         pass
