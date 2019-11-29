a6ab088e0d6b6ac72b1d183db120b04bd71f9746
AndreiRoibu <aroibu1@gmail.com>
2019-11-29 14:40:26 +0000

    modified readme, updated analytical solution and adapted model_fitting for class system

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-# PARA_PDE
-Pavan, Alister, Rebecca, Andrei 
+# FitzHugh-Nagumo Reaction-Diffusion Equation Solver
 
-###Reaction diffusion equation solver for the FitzHugh-Nagumo model
+## Motivation
+This projects aims at providing a solver for the FitzHugh-Nagumo Reaction-Diffusion nonlinear PDE, as well as code which can extract the PDE's parameters.
+
+This code was created as an assignment for the _Modelling & Scientific Computing_ module, at the _EPSRC CDT in Sustainable Approached to Biomedical Sciences: Responsible and Reproducible Research - SABS R3_
+
+## Installation
+In order to install the packaged, the user will require the presence of Python3 and the [pip3](https://pip.pypa.io/en/stable/) installer. 
+
+For installation on Linux or OSX, use the following commands. This will create an environment and automatically install all the requirements.
+
+```bash
+python3 -m venv env
+source env/bin/activate
+pip install -e .
+```
+
+## Usage
+In order to run the solver, type the following commands int the activated python environment. For the **solver** type in the first command, while for the **moldel fitting function** type in the second command. 
+
+```python
+for solver
+```
+
+```python
+for model_fitting
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause) © MIT © [Alister Dale-Evans](https://github.com/alisterde), [Andrei Roibu](https://github.com/AndreiRoibu), [Pavan Chaggar](https://github.com/PavanChaggar), [Rebecca Rumney](https://github.com/Rebecca-Rumney)
+
+## References
+In the creation of this code, material was used from the following paper:
+
+```
+@article{parand2017numerical,
+title={A numerical method to solve the 1D and the 2D reaction diffusion equation based on Bessel functions and Jacobian free Newton-Krylov subspace methods},
+author={Parand, K and Nikarya, M},
+journal={The European Physical Journal Plus},
+volume={132},
+number={11},
+pages={496},
+year={2017},
+publisher={Springer}
+}
+```
+
+## Credits
+The team would like to express their gratitude to [Martin Robinson](https://github.com/martinjrobins) and [Aleksandra Ardaseva](https://www.maths.ox.ac.uk/people/aleksandra.ardaseva) for their help and support during the development of this code.
+
+## Build status
+Describe and show how to run the tests with code examples.
+
+Build status of continus integration i.e. travis, appveyor etc. Ex. - 
+
+[![Build Status](https://travis-ci.org/akashnimare/foco.svg?branch=master)](https://travis-ci.org/akashnimare/foco)
+[![Windows Build Status](https://ci.appveyor.com/api/projects/status/github/akashnimare/foco?branch=master&svg=true)](https://ci.appveyor.com/project/akashnimare/foco/branch/master)
+
+
+
+
--- model_fitting.py
+++ model_fitting.py
@@ -15,22 +15,16 @@ def ModelFit():
 
     u = solver.FN_solver(x_0 = 0, x_n = 20)
     
-    # eps = 1.0
-    # h = 0.05
-    # k = 0.2 * h**2 / eps
-    # Tf = 42.0
     var = 0.0 # scale of random normal noise
 
     np.random.seed(100) #setting the seed to ensure cosnsitency in random number generation
 
-    # x = np.arange(0+h, 20-h, h)
+    time = np.linspace(0,solver.end_time,solver.k_N)
 
-    # numsteps = int(np.ceil(Tf/k)/10)
+    T,X = np.meshgrid(time, solver.x_range)
 
-    t = np.linspace(0,1,solver.k_N)
-    
-
-    input_data = np.meshgrid(t, solver.x_range)
+    y_data = np.empty(u.shape)
+    print(y_data.shape)
 
     # Generating noisy data using the Analytical Solution and randomly normal distributed noise
 
@@ -38,51 +32,43 @@ def ModelFit():
 
     # # Define the model parameters to be fitted
 
-    # alpha = 0.2
-    # beta = 1.0 
-    # gama = 1.0
+    # y_data = AnalyticalSolution(input_data[1], input_data[0], alpha=solver.alpha) + var * np.random.randn(len(solver.x_range), len(t))
+    y_data = AnalyticalSolution(X, T, alpha=solver.alpha) + var * np.random.randn(len(solver.x_range), len(time))
 
-    y_data = AnalyticalSolution(input_data[0], input_data[1], alpha=solver.alpha) + var * np.random.randn(len(solver.x_range), len(t))
-    y_solver = solver.FN_solver(x_0 = 0, x_n = 20)
+    y_solver = solver.FN_solver(x_0 = 0, x_n = 20, boundary_conditions=[0,1])
 
     plt.figure()
-    plt.plot(solver.x_range, y_data[:,0],label='analytical')
+    plt.plot(solver.x_range, y_data[:,-1],'*r',label='analytical')
     plt.plot(solver.x_range, y_solver[:,-1],label='solver')
     plt.legend()
     plt.show()
 
+    def cost_function(parameters):
 
-    # def cost_function(parameters):
+        '''
+        ADD DESCRIPTION!
+        '''
 
-    #     '''
-    #     ADD DESCRIPTION!
-    #     '''
+        # y_solver = solve_fitzhuge_nagumo(alpha=parameters[0],beta=parameters[1],gamma=parameters[2])
+        y_data = AnalyticalSolution(input_data[0], input_data[1]) + var * np.random.randn(len(solver.x_range), len(t))
+        y_solver = solver.FN_solver(x_0 = 0, x_n = 20,alpha=parameters[0])
 
-    #     # y_solver = solve_fitzhuge_nagumo(alpha=parameters[0],beta=parameters[1],gamma=parameters[2])
-    #     y_data = AnalyticalSolution(input_data[0], input_data[1]) + var * np.random.randn(len(solver.x_range), len(t))
-    #     y_solver = solver.FN_solver(x_0 = 0, x_n = 20,alpha=parameters[0])
-
-    #     return np.sum(np.power(y_solver - y_data, 2))
+        return np.sum(np.power(y_solver - y_data, 2))
 
     # # Randomly initializing the model parameters.
     # # As stated in the original paper, alpha needs to be within (0, 0.5)
 
-    # alpha0 = np.random.normal(1) * var
-    # while alpha0 < 0 or alpha0 > 0.5:
-    #     alpha0 = np.random.normal(1) * var
-    # # beta0 = np.random.normal(1) * var 
-    # # gama0 = np.random.normal(1) * var
+    alpha0 = np.random.normal(1) * var
+    while alpha0 < 0 or alpha0 > 0.5:
+        alpha0 = np.random.normal(1) * var
+    # beta0 = np.random.normal(1) * var 
+    # gama0 = np.random.normal(1) * var
 
-    # # original_parameters = [alpha0, beta0, gama0]
-    # original_parameters=np.array([alpha0])
-
-    # for method in ['nelder-mead', 'bfgs']:
-    #     res = minimize(cost_function, original_parameters, method=method, options={'disp': True})
-    #     print(res.x)
-        
-    # # res2 = shgo(cost_function, bounds=[(0.0,0.5),(-10,10),(-10,10)], options={'disp': True})
-    # res2 = shgo(cost_function, bounds=[(0.0,0.5)], options={'disp': True})
-    # print(res2.x)
+    # original_parameters = [alpha0, beta0, gama0]
+    original
+    shgo(cost_function, bounds=[(0.0,0.5),(-10,10),(-10,10)], options={'disp': True})
+    res2 = shgo(cost_function, bounds=[(0.0,0.5)], options={'disp': True})
+    print(res2.x)
     
 
 if __name__ == '__main__':
