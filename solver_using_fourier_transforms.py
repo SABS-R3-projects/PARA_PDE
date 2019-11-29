734be5d32a2ee91608a07e06ba8d8b9135f92e52
rumneyr <rebecca.rumney@wolfson.ac.uk>
2019-11-29 14:44:12 +0000

    fourier transforms doing what they should do

--- solver_using_fourier_transforms.py
+++ solver_using_fourier_transforms.py
@@ -8,56 +8,43 @@ def initial_conditions(x, a):
     return AnalyticalSolution.AnalyticalSolution(x, 0, alpha=a)
 
 
-#def delta_diffusion(x, u, delta_t, max_x):
-#    fourier = np.fft.fft(u)
-#    N = len(x)
-#    delta_x = N/max_x
-#    delta_a = []
-#    for j in range(0, N):
-#        k_j = j * np.pi/(N * delta_x)
-#        delta_a.append(np.exp(-np.power(k_j, 2)*delta_t))
-#    delta_fourier = delta_a*fourier
-#    return delta_fourier
-
-
-#def delta_extra(x, u, delta_t, max_x):
-#    dg_dt = u * (1- u) * (u - alpha)
-
-
-#def delta_u(x, u, delta_t, max_x):
-#    d_u = delta_diffusion(x, u, delta_t, max_x) + delta_extra(x, u, delta_t, max_x)
-#    return d_u
-
-
-def fitzhugh_nagumo_solver(alpha=0.2, gamma=1, delta=1, max_x=20, max_t=10):
+def fitzhugh_nagumo_solver(alpha=0.2, gamma=1, delta=1, max_x=1, max_t=10):
     # Initial conditions: u(x,0) = I(x)
     # Boundry conditions u(0, t) = B = u(L, t)
     # Fourier Conditions: if x' = x + 2nL then u(x', t) = u(x, t) for all integers n
     L = max_x
-    meshpoints = 10
-    x = np.linspace(0, L, meshpoints)
-    numsteps = 50
+    meshpoints = 160
+    x = np.linspace(-L, L, meshpoints)
+    y = np.linspace(0, L, round(meshpoints/4))
+    numsteps = 10
     delta_t = max_t/numsteps
 
-    initial = initial_conditions(x, alpha)
+    initial = initial_conditions(10*x, alpha)
+    # nought = np.zeros(2)
+    nought = []
+    initial = np.hstack((np.flip(initial), nought, initial))
+    x = np.linspace(0, L, meshpoints)
+    x = np.hstack((np.flip(-x), x))
     u_current = initial
 
+    u_relevant = u_current[-len(y):]
+
     fig, ax = plt.subplots()
-    ln, = plt.plot(x, u_current)
+    ln, = plt.plot(y, u_relevant)
     ax.set_ylim(0, 1.1)
 
-    all_u = []
+    all_u = [initial]
 
     def delta_diffusion(u):
         fourier = np.fft.fft(u)
         delta_x = meshpoints / max_x
         delta_a = []
-        for j in range(0, meshpoints):
-            k_j = j * np.pi / (meshpoints * delta_x)
-            delta_a.append(np.exp(-np.power(k_j, 2) * delta_t))
-        delta_fourier = delta_a * fourier
+        j = np.linspace(-len(x)/2, len(x)/2, len(x))
+        k_j = j * np.pi / (len(x)/2) * delta_x
+        a = np.exp(-np.power(k_j, 2) * delta_t)
+        delta_fourier = a * fourier
         delta_f = np.fft.ifft(delta_fourier)
-        # print(delta_f)
+        print(delta_f)
         return delta_f
 
     def delta_extra(u):
@@ -78,6 +65,7 @@ def fitzhugh_nagumo_solver(alpha=0.2, gamma=1, delta=1, max_x=20, max_t=10):
 
     def delta_u(u):
         d_u = delta_diffusion(u) + delta_extra(u)
+        # d_u = delta_diffusion(u)
         # print(d_u)
         return d_u
 
@@ -86,20 +74,22 @@ def fitzhugh_nagumo_solver(alpha=0.2, gamma=1, delta=1, max_x=20, max_t=10):
         for i in range(1):
             u_new = u_current + delta_u(u_current)
             u_current[:] = u_new
-            all_u.append(u_current)
+            #u_current[0], u_current[-1] = 1, 1
+            u_relevant[:] = u_current[-len(y):]
+            all_u.append(u_relevant)
             print(u_current)
 
-        ln.set_data(x, u_current)
+        ln.set_data(y, u_relevant)
         ax.set_title('t = {}'.format(10 * frame * delta_t))
         # print(frame)
         return ln,
 
-    ani = FuncAnimation(fig, update, frames=1, interval=30, blit=False, repeat=False)
+    ani = FuncAnimation(fig, update, frames=max_t, interval=30, blit=False, repeat=False)
     plt.show()
 
     plt.plot(x, u_current)
     plt.plot(x, initial)
     plt.show()
 
-
 fitzhugh_nagumo_solver()
+#print(initial_conditions(-1, 0.2))
