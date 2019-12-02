3e8aeedf53173beded5af708fe1bb5fd065d2189
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2019-12-02 17:14:55 +0000

    Debugging :bug:

--- test_iterative_PDE.py
+++ test_iterative_PDE.py
@@ -25,9 +25,11 @@ class test_Iterative_PDE_solver(unittest.TestCase):
         """Tests the PDE solver outputs a argument of the correct dimensions
         """
         trial = FitzHugh_Nagumo_solver()
+        expected_Y_Dimesnion = 80
         u = trial.FN_solver(x_0=0, x_n=20, step_size  = 0.05,
-                 time_steps = 80, alpha = 0.13)
+                 time_steps = expected_Y_Dimesnion, alpha = 0.13)
+        expected_X_Dimesnion = 1 + 20/(20/expected_Y_Dimesnion) #adding 1 to include x_0 and cacluating expected dimensiosn from the number of times the step size goes into the final x_n value
         Dimensions = u.shape
-        self.assertEqual((trial.N_dim, trial.k_N), Dimensions)
+        self.assertEqual((expected_X_Dimesnion, expected_Y_Dimesnion), Dimensions)
 if __name__ == '__main__':
     unittest.main()
